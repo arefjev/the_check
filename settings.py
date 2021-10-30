52cc2079f13e2dd32dec821732cc478c4d15fb88
Egor Arefjev <arefjev1980@gmail.com>
2021-10-31 01:39:20 +0400

    Add python-decouple library to project
    Смотрю документацию сервиса proverkacheka

---
+++ settings.py
@@ -0,0 +1,5 @@
+from decouple import config
+
+
+API_KEY = config('API_KEY')
+BASE_URL = 'https://proverkacheka.com/api/v1/check/get'
--- the_check.py
+++ the_check.py
@@ -1,6 +1,15 @@
+import requests
+from settings import API_KEY, BASE_URL
+
 
 def initial_commit(name):
-    print(f'Initial commit, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Initial commit, {name}')
+
+
+def get_check(api_key, base_url, fn, fd, fp, t, n, s):
+    request_url = base_url + '/?'
+    api_request = requests.post(request_url).json()
+    pass
 
 
 if __name__ == '__main__':
